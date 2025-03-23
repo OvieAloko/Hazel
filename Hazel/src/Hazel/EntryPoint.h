681e59abd07cd1c3937b1a93f4c0c0c4829d80aa
Ovie Aloko <stephenaloko7@gmail.com>
2025-03-23 23:22:25 +0000

    Setup Entry Point, Logging and Premake.

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialised Log!");
+	HZ_CORE_INFO("Hello");
+
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
